69ffcce19458e34f17821c24fe3122b902931b44
Anastasia Voitsenya <rufousrat@gmail.com>
2019-07-21 22:07:03 +0300

    Add chart.js example

--- src/app/data-pages/components/data-meteo/data-meteo.component.ts
+++ src/app/data-pages/components/data-meteo/data-meteo.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import Chart from 'chart.js';
+import dataSet from '../../../../assets/data/discharge/1961.json';
+
 
 @Component({
   selector: 'app-data-meteo',
@@ -11,11 +14,68 @@ export class DataMeteoComponent implements OnInit {
 
   public years = [];
 
+  @ViewChild('chartLine') chartLine: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+    this.getYears();
+    this.createChart();
+  }
+
+
+
+
+  public createChart() {
+
+    const donutCtx = this.chartLine.nativeElement.getContext('2d');
+
+    const output = Object.entries(dataSet).map(([key, value]) => ({key, value}));
+
+    const labelsL = output.map(item => item.key);
+    const dataL = output.map(item => item.value.Q_Lusyanka);
+
+    const config = {
+      type: 'line',
+      data: {
+        labels: labelsL,
+        datasets: [{
+          label: 'расход воды в р.Лусянке в створе д.Черники',
+          backgroundColor: '#193089',
+          borderColor: '#4eaeee',
+          data: dataL,
+          fill: true,
+        }
+        ]
+      },
+      options: {
+        responsive: true,
+        title: {
+          display: false,
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+        },
+        scales: {
+          xAxes: [{
+            stacked: true
+          }],
+          yAxes: [{
+            stacked: true
+          }]
+        }
+      }
+    };
+
+    const test = new Chart(donutCtx, config);
+
+  }
+
+  private getYears() {
     for (let i = this.startDate; i <= this.endDate; i++) {
       this.years.push(i);
     }
   }
+
 }
